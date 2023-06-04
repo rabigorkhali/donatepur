718b1bda0fe3a7c5d56dd1dd3c7b5b4e488468c0
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-06-04 21:36:35 +0545

    initial commits

---
+++ app/Models/Voyager/CampaignCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Voyager;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CampaignCategory extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Voyager/Setting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Voyager;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Events\SettingUpdated;
+
+class Setting extends Model
+{
+    protected $table = 'settings';
+
+    protected $guarded = [];
+
+    public $timestamps = false;
+
+    protected $dispatchesEvents = [
+        'updating' => SettingUpdated::class,
+    ];
+}
