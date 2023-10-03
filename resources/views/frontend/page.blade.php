f84693d61da7c782dffabb279399e515517dfdd2
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-10-03 14:17:42 +0545

    dfgdfgdf

--- resources/views/frontend/page.blade.php
+++ resources/views/frontend/page.blade.php
@@ -3,19 +3,21 @@
 @section('content')
     <div class="main-content">
         <!-- Section: inner-header -->
-        
-        <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-stellar-background-ratio="0.5" data-bg-img="{{ asset('/public/uploads') . '/' . imageName($pageDetails->image, '-cropped') }}" style="background-image: url('{{ asset('/public/uploads') . '/' . imageName($pageDetails->image, '-cropped') }}'); background-position: 50% 61px;">
+
+        <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-stellar-background-ratio="0.5"
+            data-bg-img="{{ asset('/public/uploads') . '/' . imageName($pageDetails->image, '-banner') }}"
+            style="background-image: url('{{ asset('/public/uploads') . '/' . imageName($pageDetails->image, '-banner') }}'); background-position: 50% 61px;">
             <div class="container pt-100 pb-50">
-              <!-- Section Content -->
-              <div class="section-content">
-                <div class="row"> 
-                  <div class="col-md-12">
-                    <h3 class="title text-white">{{$pageDetails->title}}</h3>
-                  </div>
+                <!-- Section Content -->
+                <div class="section-content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="title text-white">{{ $pageDetails->title }}</h3>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </section>
+        </section>
 
 
 
@@ -24,7 +26,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <img class="pull-left mr-15 thumbnail" src="http://placehold.it/430x240" alt="">
+                        {{-- <img class="pull-left mr-15 thumbnail" src="http://placehold.it/430x240" alt=""> --}}
+                        {{-- <img class="pull-left mr-15 thumbnail" src="http://placehold.it/430x240" alt=""> --}}
+                        <img class="pull-left mr-15 thumbnail"
+                            src="{{ asset('/public/uploads') . '/' . imageName($pageDetails->image, '-cropped') }}"
+                            alt="">
                         <p> {!! $pageDetails->body !!} </p>
                         <div class="clearfix"></div>
                     </div>
