dc05ab446ca3cce053c6d33a5461f7f1efc7e1f9
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-10-08 10:20:24 +0545

    check views

--- resources/views/email/donation-given.blade.php
+++ resources/views/email/donation-given.blade.php
@@ -389,6 +389,7 @@
                                                             <p
                                                                 style="margin: 10; font-size: 14px; text-align: center; mso-line-height-alt: 25.2px;">
                                                                 <span style="font-size:14px;">
+                                                                    Regards, <br> <b>Donatepur - The Place of hopes </b> <br>
                                                                     {{ setting('site.site_email') }},{{ setting('site.mobile_number') }}<br>
                                                                     {{ setting('site.site_address') }}<br>
                                                                     {{ setting('site.copy_right_footer_text') }}
