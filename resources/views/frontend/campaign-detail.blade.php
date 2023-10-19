4b3605c9d820912938dd90c5613d354a8432f4a0
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-10-19 12:41:55 +0545

    dfgdfg

--- resources/views/frontend/campaign-detail.blade.php
+++ resources/views/frontend/campaign-detail.blade.php
@@ -93,7 +93,7 @@
                             <div class="target-information pb-sm-20 bg-light pr-0 pb-50 pl-0">
                                 <div class="col-md-6">
                                     <div class="col-md-12 text-center">
-                                        <img width="200" class="img-circle" alt="" style="margin: 7px;"
+                                        <img width="160" class="img-circle" alt="" style="margin: 7px;"
                                             src="{{ asset('/public/uploads') . '/' . imageName($campaignDetails?->owner?->profile_picture, '-medium') }}">
                                     </div>
                                     <div class="col-md-12 text-center">
