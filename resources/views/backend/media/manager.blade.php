8ffc92bc09d27cb28e43ecf911d19fb56189d5c1
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-11-07 15:39:32 +0545

    dfg

--- resources/views/backend/media/manager.blade.php
+++ resources/views/backend/media/manager.blade.php
@@ -129,9 +129,9 @@
                 <div id="file_loader" v-if="is_loading">
                     <?php $admin_loader_img = Voyager::setting('admin.loader', ''); ?>
                     @if($admin_loader_img == '')
-                    <img src="{{ voyager_asset('images/logo-icon.png') }}" alt="Voyager Loader">
+                    <img src="{{ asset('/public/uploads') . '/' . setting('site.logo') }}" alt="Donatepur Loader">
                     @else
-                    <img src="{{ Voyager::image($admin_loader_img) }}" alt="Voyager Loader">
+                    <img src="{{ asset('/public/uploads') . '/' . setting('site.logo') }}" alt="Donatepur Loader">
                     @endif
                     <p>{{ __('voyager::media.loading') }}</p>
                 </div>
