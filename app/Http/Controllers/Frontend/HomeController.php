93d4c16ece19f3e736a0d80489a30439f87c118e
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-11-08 14:23:05 +0545

    test cicd

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -172,7 +172,7 @@ class HomeController extends FrontendBaseController
     public function postList(Request $request)
     {
         dump('test cicd');
-        dump('test cic222d');
+        dump('test cic22q2d');
         try {
             $data = array();
             $keyword = trim($request->get('title'));
