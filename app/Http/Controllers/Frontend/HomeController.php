192487bfac36cbf57b647971325c4323213c0aa4
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-11-17 12:55:46 +0545

    testsdf

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -172,9 +172,9 @@ class HomeController extends FrontendBaseController
     public function postList(Request $request)
     {
         try {
-            dump('testss');
-            dump('testss');
-            dump('testss');
+            // dump('testss');
+            // // dump('testss');
+            // dump('testss');
             $data = array();
             $keyword = trim($request->get('title'));
             $category = trim($request->get('category'));
