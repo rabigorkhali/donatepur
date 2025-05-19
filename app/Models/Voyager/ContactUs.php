55679158b0e25d02d9efd1637185e00c396ceb01
unknown <Rabi Gorkhali>
2025-05-19 12:11:00 +0545

    filllable contact us

--- app/Models/Voyager/ContactUs.php
+++ app/Models/Voyager/ContactUs.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactUs extends Model
 {
-    
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'message',
+    ];
 }
